d36aad7fbb0b8956395547d2d858d378d7faf5bf
mdAlnahian <muhammedalnahian@gmail.com>
2022-04-25 22:42:17 +0600

    adding steps

---
+++ _server_deploy.js
@@ -0,0 +1,13 @@
+//---------------------
+// One time in your pc
+//------------------------
+//1.create heroku account
+//2.verify email
+//3.install heroku cli
+//4.heroku login
+
+//------------------
+//one time for each project
+//-----------------------
+//1.heroku create
+//2.
